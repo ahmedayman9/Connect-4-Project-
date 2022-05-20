de20f5c5a392476da7061b84d8f1a43dc3ca08b8
mona690 <66241299+mona690@users.noreply.github.com>
2022-05-20 18:35:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -372,3 +372,70 @@ int hasEmptyCol(char *board, int col)       // bymshi mn ta7t l fo2 fi nfs el co
     }
     return 0;
 }
+volatile unsigned long FallingEdges = 0;
+void EdgeCounter_Init(void){                          
+  SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
+	
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0        
+	
+  FallingEdges = 0;             // (b) initialize counter
+  
+  GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x11;    //     PF4 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x11;    //     PF4 falling edge event
+  GPIO_PORTF_ICR_R = 0x11;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
+  EnableInterrupts();           // (i) Clears the I bit
+}
+
+void GPIOPortF_Handler(void){
+		flag=0;
+
+		FallingEdges = FallingEdges + 1;
+		if(GPIO_PORTF_RIS_R&(1<<4)){
+	    GPIO_PORTF_ICR_R = 0x10;
+			if(flag2){
+				cols++;
+				if (cols > 6)                    // aw n7ot %7
+						cols = 0;
+			}
+			else{
+				Nokia5110_SetCursor(0, k + 2);
+				Nokia5110_OutString("  ");
+				k += 2;
+				if (k > 2)
+						k = 0;
+				Nokia5110_SetCursor(0, k + 2);
+				Nokia5110_OutString("->");
+				}
+
+		}
+	if(GPIO_PORTF_RIS_R&(1<<0)){
+	    GPIO_PORTF_ICR_R = 0x01;
+			flag = 1;
+	}
+
+}
+
+void Delay100ms(unsigned long count)
+{
+    unsigned long volatile time;
+    while (count)
+    {
+        time = 72724; // 0.1sec at 80 MHz
+        while (time)
+        {
+            time--;
+        }
+        count--;
+    }
+}
